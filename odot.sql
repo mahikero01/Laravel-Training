d74dfd696c95465e9e3c5eb4da4f7b4d88ac6bb5
Rico - Laptop <federicoparassarmiento@yahoo.com>
2014-10-09 23:34:39 +0800

    Up

--- odot.sql
+++ odot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2014 at 06:51 AM
+-- Generation Time: Oct 09, 2014 at 05:33 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,20 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_09_152014_create_todo_lists_table', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `todo_lists`
 --
 
 CREATE TABLE IF NOT EXISTS `todo_lists` (
-`id` int(11) NOT NULL,
-  `name` text NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `todo_lists`
---
-
-INSERT INTO `todo_lists` (`id`, `name`) VALUES
-(1, 'Rico lists');
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Indexes for dumped tables
@@ -46,7 +59,7 @@ INSERT INTO `todo_lists` (`id`, `name`) VALUES
 -- Indexes for table `todo_lists`
 --
 ALTER TABLE `todo_lists`
- ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `todo_lists_name_unique` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -56,7 +69,7 @@ ALTER TABLE `todo_lists`
 -- AUTO_INCREMENT for table `todo_lists`
 --
 ALTER TABLE `todo_lists`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
