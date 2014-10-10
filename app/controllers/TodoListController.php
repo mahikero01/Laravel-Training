de3e9f828fa171404a4c1340541aa89f95b7a89c
Rico - Laptop <federicoparassarmiento@yahoo.com>
2014-10-10 13:22:18 +0800

    Up

--- app/controllers/TodoListController.php
+++ app/controllers/TodoListController.php
@@ -4,7 +4,7 @@ class TodoListController extends \BaseController {
 	
 	public function __construct()
 	{
-		$this->beforeFilter('csrf', array('on' => 'post' ));	
+		$this->beforeFilter('csrf', array('on' => ['post', 'put'] ));	
 	}
 
 
@@ -80,7 +80,8 @@ class TodoListController extends \BaseController {
 	 */
 	public function edit($id)
 	{
-		//
+		$list = TodoList::findOrFail($id);
+		return View::make('todos.edit')->withList($list);
 	}
 
 
@@ -92,7 +93,24 @@ class TodoListController extends \BaseController {
 	 */
 	public function update($id)
 	{
-		//
+		//rules
+		$rules = array(
+			'name' => array('required', 'unique:todo_lists,name')
+		);
+		
+		//validator
+		$validator = Validator::make(Input::all(), $rules);
+		
+		//test
+		if ( $validator->fails() ) {
+			return Redirect::route('todos.edit', $id)->withErrors($validator)->withInput();
+		}
+		
+		$name = Input::get('name');
+		$list = TodoList::findOrFail($id);
+		$list->name = $name;
+		$list->update();
+		return Redirect::route('todos.index')->withMessage('List Was Updated');
 	}
 
 
--- app/views/todos/index.blade.php
+++ app/views/todos/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.main')
 @section('content')
 	<h2>Show All Todo Lists</h2>
-	<ul>
 		@foreach ($todo_lists as $list)
-		<li>{{{ $list->name }}}</li>
+			<h4>{{ link_to_route('todos.show', $list->name,[$list->id]) }}</h4>
+			<ul class="no-bullet button-group">
+				<li>
+					{{ link_to_route('todos.edit', 'edit', [$list->id], ['class' => 'tiny button']) }}
+				</li>
+			</ul>
 		@endforeach
-	</ul>
+		{{ link_to_route('todos.create', 'Create a New List', null, ['class' => 'success button']) }}
 @stop
